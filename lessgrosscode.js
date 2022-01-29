b53196e42fc4a89a2440508064449af4b5e6ba5d
Jaron Stevenson <39342803+jdipity@users.noreply.github.com>
2022-01-28 20:14:08 -0500

    Start at current date

--- lessgrosscode.js
+++ lessgrosscode.js
@@ -934,7 +934,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function (e) {
 			s = e % 100;
 		return e + (a[(s - 20) % 10] || a[s] || a[0])
 	}
-	var qa = new Date(2021, 5, 19, 0, 0, 0, 0);
+	var qa = new Date(2021, 1, 28, 0, 0, 0, 0);
 
 	function Ea(e, a) {
 		var s = new Date(e),
@@ -1041,7 +1041,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function (e) {
 		r(t, c(HTMLElement));
 		var a = m(t);
 
-		function t() {
+		function t() { //Important
 			var e;
 			s(this, t), n(p(e = a.call(this)), "tileIndex", 0), n(p(e), "rowIndex", 0), n(p(e), "solution", void 0), n(p(e), "boardState", void 0), n(p(e), "evaluations", void 0), n(p(e), "canInput", !0), n(p(e), "gameStatus", Pa), n(p(e), "letterEvaluations", {}), n(p(e), "$board", void 0), n(p(e), "$keyboard", void 0), n(p(e), "$game", void 0), n(p(e), "today", void 0), n(p(e), "lastPlayedTs", void 0), n(p(e), "lastCompletedTs", void 0), n(p(e), "hardMode", void 0), n(p(e), "dayOffset", void 0), e.attachShadow({
 				mode: "open"
@@ -1049,7 +1049,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function (e) {
 			var o = ma();
 			return e.lastPlayedTs = o.lastPlayedTs, !e.lastPlayedTs || Ea(new Date(e.lastPlayedTs), e.today) >= 1 ? (e.boardState = new Array(6).fill(""), e.evaluations = new Array(6).fill(null), e.solution = function (e) {
 				var a;
-				return a = (Aa(e) - 207) % va.length, va[a]
+				return a = (Aa(e)) % va.length, va[a]
 			}(e.today), e.dayOffset = Aa(e.today), e.lastCompletedTs = o.lastCompletedTs, e.hardMode = o.hardMode, e.restoringFromLocalStorage = !1, ha({
 				rowIndex: e.rowIndex,
 				boardState: e.boardState,
